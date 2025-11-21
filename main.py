92cea7b7583a48f7bde6ce31e93d3f38006f1e25
Electrify3 <electrify312@gmail.com>
2025-11-21 12:16:28 +0530

    minor chnages in main.py

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ bot = telegram.Client()
 async def on_message(message: telegram.Message):
     text = message.text.lower()
 
-    if text in ['hi', 'hi', 'hello', 'hey']:
-        await bot.send(message.chat_id, f"{text.capitalize()} {message.author.first_name}!, How are you.")
+    replies: dict[tuple, str] = {
+    ('hi', 'hello', 'hey'): f'{text.capitalize()} {message.author.first_name}!, How are you.',
+    ('gm', 'good morning'): f'Good Morning {message.author.first_name}, have a great day ahead <3',
+    ('gn', 'good night'): f'Good Night {message.author.first_name}, have sweet dreams :)'
+}
+
+    for triggers in replies:
+        if text in triggers:
+            await bot.send(message.chat_id, replies[triggers])
     
 
 bot.run(os.environ['TOKEN'])
