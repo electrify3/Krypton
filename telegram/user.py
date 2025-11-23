d9f5912104681731749d3de3f4056b60a67b54f2
Electrify3 <electrify312@gmail.com>
2025-11-23 13:12:31 +0530

    added client as argument and took chat object instead of chat_id only

--- telegram/user.py
+++ telegram/user.py
@@ -1,5 +1,11 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .client import Client
+
 class User:
-    def __init__(self, data: dict):
-        self.id = data["id"]
-        self.first_name = data["first_name"]
-        self.username = data.get("username")
+    def __init__(self, client: Client, user_json: dict):
+        self._client = client
+        self.id = user_json["id"]
+        self.name = user_json["first_name"]
+        self.username = user_json.get("username")
